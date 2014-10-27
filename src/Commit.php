82c28dc3e686a279a984180a4632297b0a4bada4
Stijn Vannieuwenhuyse <stijn@vannieuwenhuyse.net>
2014-10-27 23:58:28 +0100

    docblocks for Commit interface

--- src/Commit.php
+++ src/Commit.php
@@ -4,6 +4,14 @@ namespace Nortuni\Estoria;
 
 interface Commit
 {
+    /**
+     * @param Commit $other
+     * @return boolean
+     */
     public function equals(Commit $other);
+
+    /**
+     * @return CommittableEvent[]
+     */
     public function getEvents();
 }
